e964901b762b2df2c5df5408dc4713c4ac30b1cc
nkw0926 <85555575+nkw0926@users.noreply.github.com>
2023-08-02 14:47:37 +0900

    [create prj]

---
+++ src/test/java/com/example/finalPrj/FinalPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
